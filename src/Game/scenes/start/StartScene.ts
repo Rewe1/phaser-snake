abb8e62cb9afc5cec621da3ef8215c5c60efb53c
Rewe <kewyn.ryan@gmail.com>
2020-08-05 12:02:29 -0300

    Added start button

--- src/Game/scenes/start/StartScene.ts
+++ src/Game/scenes/start/StartScene.ts
@@ -1,5 +1,8 @@
+import * as iVector2D_m from '../Vector2D';
+
 class StartScene extends Phaser.Scene
 {
+    startButton: Phaser.GameObjects.Rectangle;
     constructor()
     {
         super('Start');
@@ -7,13 +10,28 @@ class StartScene extends Phaser.Scene
 
     create()
     {
-        this.add.text(16, 16, 'Start Scene started...');
+		this.cameras.main.setBackgroundColor('#dedede');
+        this.startButton = this.add.rectangle(window.innerWidth/2, window.innerHeight/2, 192, 64, 0x808080);
+        this.startButton.setInteractive();
+        this.startButton.on('pointerdown', () =>
+        {
+            this.scene.start('Game');
+        });
+        this.add.text(window.innerWidth/2 -192/2, window.innerHeight/2 -64/2, 'Start Game',
+        {
+            fontSize: 22,
+            fixedWidth: 192,
+            padding:
+            {
+                top: 64/2 - 22/2,
+            },
+            align: 'center',
+            maxLines: 0,
+        });
     }
 
     update()
     {
-        if(window.shouldStart)
-            this.scene.start('Game');
     }
 }
 
